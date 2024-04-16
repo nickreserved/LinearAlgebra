1c93861840598f1a1cd996fcf579dce11cecd4af
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2024-04-16 16:07:49 +0300

    Added shortcut AMD reordering

--- tests/MGroup.LinearAlgebra.Tests/Reordering/SparsityPatternSymmetricTests.cs
+++ tests/MGroup.LinearAlgebra.Tests/Reordering/SparsityPatternSymmetricTests.cs
@@ -1,87 +1,101 @@
-﻿using System;
-using MGroup.LinearAlgebra.Matrices;
-using MGroup.LinearAlgebra.Output;
-using MGroup.LinearAlgebra.Reordering;
-using MGroup.LinearAlgebra.Tests.TestData;
-using MGroup.LinearAlgebra.Tests.Utilities;
-using Xunit;
-
 namespace MGroup.LinearAlgebra.Tests.Reordering
 {
-    /// <summary>
-    /// Tests for <see cref="SparsityPatternSymmetric"/>.
-    /// Authors: Serafeim Bakalakos
-    /// </summary>
-    public static class SparsityPatternSymmetricTests
-    {
-        private static readonly MatrixComparer comparer = new MatrixComparer(1E-13);
+	using System;
 
-        [Fact]
-        private static void TestConnectIndices()
-        {
-            int n = GlobalMatrixAssembly.GlobalOrder;
-            var dense = Matrix.CreateFromArray(GlobalMatrixAssembly.GlobalMatrix);
-            var pattern = SparsityPatternSymmetric.CreateEmpty(n);
-            pattern.ConnectIndices(GlobalMatrixAssembly.GlobalIndices1, true);
-            pattern.ConnectIndices(GlobalMatrixAssembly.GlobalIndices2, true);
-            pattern.ConnectIndices(GlobalMatrixAssembly.GlobalIndices3, true);
+	using MGroup.LinearAlgebra.Matrices;
+	using MGroup.LinearAlgebra.Output;
+	using MGroup.LinearAlgebra.Reordering;
+	using MGroup.LinearAlgebra.Tests.TestData;
+	using MGroup.LinearAlgebra.Tests.Utilities;
 
-            for (int i = 0; i < n; ++i)
-            {
-                for (int j = 0; j < n; ++j)
-                {
-                    bool denseHasZero = dense[i, j] == 0.0;
-                    bool patternHasZero = !pattern.IsNonZero(i, j);
-                    Assert.True(patternHasZero == denseHasZero);
-                }
-            }
-        }
+	using Xunit;
 
-        [Fact]
-        private static void TestReorderingAmdCSparseNet()
-        {
-            var pattern = SparsityPatternSymmetric.CreateFromDense(Matrix.CreateFromArray(SparsePosDef10by10.Matrix));
-            var orderingAlg = new OrderingAmdCSparseNet();
-            (int[] permutation, bool oldToNew) = orderingAlg.FindPermutation(pattern);
-            comparer.AssertEqual(SparsePosDef10by10.MatlabPermutationAMD, permutation);
-        }
+	/// <summary>
+	/// Tests for <see cref="SparsityPatternSymmetric"/>.
+	/// </summary>
+	public static class SparsityPatternSymmetricTests
+	{
+		private static readonly MatrixComparer comparer = new MatrixComparer(1E-13);
 
-        [SkippableFact]
-        private static void TestReorderingAmdSuiteSparse()
-        {
-            Skip.IfNot(TestSettings.TestSuiteSparse, TestSettings.MessageWhenSkippingSuiteSparse);
+		[Fact]
+		private static void TestConnectIndices()
+		{
+			int n = GlobalMatrixAssembly.GlobalOrder;
+			var dense = Matrix.CreateFromArray(GlobalMatrixAssembly.GlobalMatrix);
+			var pattern = SparsityPatternSymmetric.CreateEmpty(n);
+			pattern.ConnectIndices(GlobalMatrixAssembly.GlobalIndices1, true);
+			pattern.ConnectIndices(GlobalMatrixAssembly.GlobalIndices2, true);
+			pattern.ConnectIndices(GlobalMatrixAssembly.GlobalIndices3, true);
 
-            var pattern = SparsityPatternSymmetric.CreateFromDense(Matrix.CreateFromArray(SparsePosDef10by10.Matrix));
-            var orderingAlg = new OrderingAmdSuiteSparse();
-            (int[] permutation, bool oldToNew) = orderingAlg.FindPermutation(pattern);
-            comparer.AssertEqual(SparsePosDef10by10.MatlabPermutationAMD, permutation);
-        }
+			for (int i = 0; i < n; ++i)
+			{
+				for (int j = 0; j < n; ++j)
+				{
+					bool denseHasZero = dense[i, j] == 0.0;
+					bool patternHasZero = !pattern.IsNonZero(i, j);
+					Assert.True(patternHasZero == denseHasZero);
+				}
+			}
+		}
 
-        [SkippableFact]
-        private static void TestReorderingCamdSuiteSparse()
-        {
-            Skip.IfNot(TestSettings.TestSuiteSparse, TestSettings.MessageWhenSkippingSuiteSparse);
+		[Fact]
+		private static void TestReorderingAmdCSparseNet()
+		{
+			var pattern = SparsityPatternSymmetric.CreateFromDense(Matrix.CreateFromArray(SparsePosDef10by10.Matrix));
+			var orderingAlg = new OrderingAmdCSparseNet();
+			(int[] permutation, bool oldToNew) = orderingAlg.FindPermutation(pattern);
+			Assert.True(!oldToNew);
+			comparer.AssertEqual(SparsePosDef10by10.MatlabPermutationAMD, permutation);
+		}
 
-            int n = SparsePosDef10by10.Order;
-            var pattern = SparsityPatternSymmetric.CreateFromDense(Matrix.CreateFromArray(SparsePosDef10by10.Matrix));
-            var orderingAlg = new OrderingCamdSuiteSparse();
-            (int[] permutation, ReorderingStatistics stats) = 
-                orderingAlg.FindPermutation(pattern, SparsePosDef10by10.ConstraintsCAMD);
+		[Fact]
+		private static void TestReorderingAmdCSparseNetFromRawArrays()
+		{
+			int order = SparsePosDef10by10.Order;
+			int[] cscColOffsets = SparsePosDef10by10.SymmetricCscColOffsets;
+			int[] cscRowIndices = SparsePosDef10by10.SymmetricCscRowIndices;
+			var orderingAlg = new OrderingAmdCSparseNet();
+			(int[] permutation, bool oldToNew) = orderingAlg.FindPermutation(order, cscRowIndices, cscColOffsets);
+			Assert.True(!oldToNew);
+			comparer.AssertEqual(SparsePosDef10by10.MatlabPermutationAMD, permutation);
+		}
 
-            var originalDiagonal = new double[n];
-            var permutedDiagonal = new double[n];
-            for (int i = 0; i < n; ++i) originalDiagonal[i] = SparsePosDef10by10.Matrix[i, i];
-            for (int i = 0; i < n; ++i) permutedDiagonal[i] = originalDiagonal[permutation[i]];
+		[SkippableFact]
+		private static void TestReorderingAmdSuiteSparse()
+		{
+			Skip.IfNot(TestSettings.TestSuiteSparse, TestSettings.MessageWhenSkippingSuiteSparse);
 
-            var writer = new Array1DWriter();
-            Console.Write("Permutation (new-to-old): ");
-            writer.WriteToConsole(permutation);
-            Console.Write("Original diagonal: ");
-            writer.WriteToConsole(originalDiagonal);
-            Console.Write("Permuted diagonal: ");
-            writer.WriteToConsole(permutedDiagonal);
+			var pattern = SparsityPatternSymmetric.CreateFromDense(Matrix.CreateFromArray(SparsePosDef10by10.Matrix));
+			var orderingAlg = new OrderingAmdSuiteSparse();
+			(int[] permutation, bool oldToNew) = orderingAlg.FindPermutation(pattern);
+			comparer.AssertEqual(SparsePosDef10by10.MatlabPermutationAMD, permutation);
+		}
 
-            comparer.AssertEqual(SparsePosDef10by10.PermutationCAMD, permutation);
-        }
-    }
+		[SkippableFact]
+		private static void TestReorderingCamdSuiteSparse()
+		{
+			Skip.IfNot(TestSettings.TestSuiteSparse, TestSettings.MessageWhenSkippingSuiteSparse);
+
+			int n = SparsePosDef10by10.Order;
+			var pattern = SparsityPatternSymmetric.CreateFromDense(Matrix.CreateFromArray(SparsePosDef10by10.Matrix));
+			var orderingAlg = new OrderingCamdSuiteSparse();
+			(int[] permutation, ReorderingStatistics stats) =
+				orderingAlg.FindPermutation(pattern, SparsePosDef10by10.ConstraintsCAMD);
+
+			var originalDiagonal = new double[n];
+			var permutedDiagonal = new double[n];
+			for (int i = 0; i < n; ++i) originalDiagonal[i] = SparsePosDef10by10.Matrix[i, i];
+			for (int i = 0; i < n; ++i) permutedDiagonal[i] = originalDiagonal[permutation[i]];
+
+			var writer = new Array1DWriter();
+			Console.Write("Permutation (new-to-old): ");
+			writer.WriteToConsole(permutation);
+			Console.Write("Original diagonal: ");
+			writer.WriteToConsole(originalDiagonal);
+			Console.Write("Permuted diagonal: ");
+			writer.WriteToConsole(permutedDiagonal);
+
+			comparer.AssertEqual(SparsePosDef10by10.PermutationCAMD, permutation);
+		}
+	}
 }
